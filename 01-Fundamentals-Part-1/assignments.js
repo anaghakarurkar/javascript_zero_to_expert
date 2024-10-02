8c113e9f70701b967c2b9caa08714918b9e899b8
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-02 15:18:46 +0100

    operators demo

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -16,3 +16,19 @@ console.log("Variable language is of type " + typeof language);
 
 //let, const and var
 //change varible type to const if value is never going to change
+
+//Basic operators
+let halfPopulation = population / 2;
+console.log(country + "'s half population is " + halfPopulation);
+
+console.log("Population of " + country + " increased by one is " + population++);
+
+let finlandPopulation = 6000000;
+console.log("Does " + country + " have more population than Finland?  " + (population > finlandPopulation));
+
+let averagePopulation = 33000000;
+console.log("Does " + country + " have less people than the avarage country ? " + (population < averagePopulation));
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language + ".";
+console.log(description);
+

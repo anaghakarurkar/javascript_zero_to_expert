7fa3662bffef5b3284a1d90196120ec5db27cd9f
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-02 13:08:17 +0100

    completed assignment 1 on values and variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,5 @@
+let country  = "United Kingdom";
+let continent = "Europe";
+let population =  69138192;
+
+console.log("You live in country " + country + ". It is part of " + continent + " with population " + population);

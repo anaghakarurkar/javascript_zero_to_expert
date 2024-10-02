6a072cc5da695ef407143f4cbf178811b6a7e519
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-02 13:59:20 +0100

    Changed variables to const as values are not going to change in future.

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,15 +1,18 @@
 //values  and variables
-let country  = "United Kingdom";
-let continent = "Europe";
+const country  = "United Kingdom";
+const continent = "Europe";
 let population =  69138192;
 
 console.log("You live in country " + country + ". It is part of " + continent + " with population " + population);
 
 //Data Types
-let isIsland = true;
-let language;
+const isIsland = true;
+const language = "English";
 
 console.log("Variable isIsland is of type " + typeof isIsland);
 console.log("Variable population is of type " + typeof population);
 console.log("Variable country is of type " + typeof country);
 console.log("Variable language is of type " + typeof language);
+
+//let, const and var
+//change varible type to const if value is never going to change

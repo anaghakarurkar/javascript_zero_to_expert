47ebf2548d3789ea3281848e0a9287277866147c
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-02 12:56:25 +0100

    Added a console log

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,8 @@
 let js = 'amazing';
-        if(js === 'amazing') {
-            alert('Javascript is FUN!');
-        }
+
+if(js === 'amazing') {
+
+        alert('Javascript is FUN!');
+    }
+    
+ console.log("In JavaScript world, variable name is in camelCase notation");

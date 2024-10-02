123fbceb9c573580deeb94bdb4a9e2f6d53b27b2
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-02 13:22:35 +0100

    typeof operator demo

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,5 +1,15 @@
+//values  and variables
 let country  = "United Kingdom";
 let continent = "Europe";
 let population =  69138192;
 
 console.log("You live in country " + country + ". It is part of " + continent + " with population " + population);
+
+//Data Types
+let isIsland = true;
+let language;
+
+console.log("Variable isIsland is of type " + typeof isIsland);
+console.log("Variable population is of type " + typeof population);
+console.log("Variable country is of type " + typeof country);
+console.log("Variable language is of type " + typeof language);

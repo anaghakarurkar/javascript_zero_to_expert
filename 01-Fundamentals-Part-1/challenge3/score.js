7346404afc43da0bdbb804d56982820ae3dacce3
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-07 21:55:11 +0100

    Modified the code

--- 01-Fundamentals-Part-1/challenge3/score.js
+++ 01-Fundamentals-Part-1/challenge3/score.js
@@ -1,12 +1,18 @@
-const scoreDolphins = (96 + 108 + 89) / 3;
-const scoreKoalas = (88 + 91 + 110) / 3;
+const scoreDolphins = (190 + 108 + 89) / 3;
+const scoreKoalas = (89 + 91 + 110) / 3;
 
-if(scoreDolphins > scoreKoalas) {
-    console.log("Dolphins win the trophy");
+console.log(`Average score of Dolphins is : ${scoreDolphins}`);
+console.log(`Average score of Koalas is: ${scoreKoalas}`);
+
+if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy ");
 } 
-else if(scoreDolphins < scoreKoalas) {
+else if(scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
     console.log("Koalas win the trophy");
 } 
-else {
+else if((scoreDolphins === scoreKoalas) && scoreDolphins >= 100 && scoreKoalas >= 100) {
     console.log("Both win the trophy");
+}
+else {
+    console.log("No one wins the trophy");
 }

e0c485d332947beef87db63638001662782739f7
anaghakarurkar <anaghakarurkar@gmail.com>
2024-10-08 14:48:08 +0100

    activating strict mode demo

---
+++ 02-Fundamentals-Part-2/stricktmodeon.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let hasDriverLicence = false;
+const passedTest = true;
+
+if(passedTest) hasDriveLicence = true;
+if(hasDriverLicence) console.log("I can drive :D");
+
+//This program should show and error about typo on line 6 saying variable not defined
---
+++ 02-Fundamentals-Part-2/strictmodeoff.js
@@ -0,0 +1,7 @@
+//This program has typo error in variable name  hasDriversLicence
+//It by default it does not show any error on console
+let hasDriversLicence = false;
+const passTest = true;
+
+if(passTest) hasDriverLicence = true;
+if(hasDriversLicence) console.log("I can drive :D");
